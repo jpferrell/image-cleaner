6e19e6369233c7450e79182eb182c3f5a51751b4
Jack <jackferrell@me.com>
2022-07-08 17:28:49 -0400

    initial commit cause my computer is acting up

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "ArgParser.hh"
+
+int main(int argc, char **argv)
+{
+  ArgParser argParse(argc, argv);
+  argParse.printFlags();
+  argParse.printFlagArgs("f");
+  std::cout << argParse << "\n";
+  
+  return 0;
+}
